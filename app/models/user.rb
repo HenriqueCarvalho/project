e1eeb8cdd0f866f45b7a49f81c2d41741570d907
Henrique Carvalho <henrique.o.carvalho@hotmail.com.br>
2015-04-16 23:11:01 -0700

    Add User Avatar Features, and New Input Name and Date Birth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_many :posts
   has_many :comments
 end
